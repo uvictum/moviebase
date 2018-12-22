085d5707975e414233b64eb3875ae9da8b68eec8
uvictum <31473681+uvictum@users.noreply.github.com>
2018-12-22 15:25:07 +0200

    Update database.php

--- app/config/database.php
+++ app/config/database.php
@@ -1,6 +1,6 @@
 <?php
-$DB_USER = 'root'; // paste user here, this user should have rights to create db
-$DB_PASSWORD = '52330c'; // paste user pass
-$DB_INSTALL = 'mysql:host=localhost'; // paste the path to mysql server
-$DB_NAME = 'moviesdb'; // paste the name of the database;
+$DB_USER = ''; // paste user here, this user should have rights to create db
+$DB_PASSWORD = ''; // paste user pass
+$DB_INSTALL = ''; // paste the path to mysql server
+$DB_NAME = ''; // paste the name of the database;
 $DB_DSN = $DB_INSTALL . ';dbname='.$DB_NAME;
